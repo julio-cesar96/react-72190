381b9246e6c8b45fea0ab6530d025bc631e3d06d
julio-cesar96 <julio_cbs96@hotmail.com>
2025-05-06 19:54:57 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css'
 import Button from './components/Button/Button'
 import Welcome from './components/Welcome/Welcome'
+import { useState } from 'react';
 
 function App() {
+  const [contador, setContador] = useState(0);
+
+  function incrementar() {
+    setContador(contador + 1);
+  }
+
+  function decrementar() {
+    setContador(contador - 1);
+  }
+
+  function resetar() {
+    setContador(0);
+  }
 
   return (
     <>
@@ -15,6 +29,16 @@ function App() {
           Clique em mim
         </span>
       </Button>
+
+      <div style={{
+        textAlign: "center",
+        backgroundColor: "#facb98",
+      }}>
+        <h1> Contador: {contador} </h1>
+        <button onClick={incrementar}> Incrementar </button>
+        <button onClick={decrementar}> Decrementar </button>
+        <button onClick={resetar}> Resetar </button>
+      </div>
     </>
   )
 }
